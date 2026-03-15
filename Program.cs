80b772b00e156b0ecb2c8946a8f9e94aa150a77c
ExploitDev9989 <miguelquiroz9989@gmail.com>
2026-03-15 14:14:26 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿namespace BST_Binary_Tree_Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode root = new TreeNode(50);
+
+            root.left = new TreeNode(30);
+            root.right = new TreeNode(70);
+
+            root.left.left = new TreeNode(20);
+            root.left.right = new TreeNode(40);
+
+            root.right.left = new TreeNode(60);
+            root.right.right = new TreeNode(80);
+
+            Program p = new Program();
+            TreeNode result = p.SearchBST(root, 60);
+
+            if (result != null)
+            {
+                Console.WriteLine("Found node: " + result.val);
+            }
+            else
+            {
+                Console.WriteLine("Value not found");
+            }
+            Console.ReadLine();
+        }
+        public TreeNode SearchBST(TreeNode root, int val)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+
+            if (root.val == val)
+            {
+                return root;
+            }
+
+            if (val < root.val)
+            {
+                return SearchBST(root.left, val);
+            }
+            else
+            {
+                return SearchBST(root.right, val);
+            }
+        }
+
+    }
+}
---
+++ TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST_Binary_Tree_Search
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int val)
+        {
+            this.val = val;
+            left = null;
+            right = null;
+        }
+    }
+}
